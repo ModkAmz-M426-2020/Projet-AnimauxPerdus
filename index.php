ddf7e3a8e1034cb996633b2944c11abd76d5c533
Aliya Mz <aliya.mz@eduge.ch>
2020-04-29 09:45:41 +0200

    Commencé la page annonce

--- index.php
+++ index.php
@@ -1,25 +1,45 @@
 <!--
  * Projet: Animaux Perdus|| M426
- * 
- * Description: site d'annonce qui permettra aux utilisateurs de poster des annonces lorsqu'ils recherchent un animal 
+ *
+ * Description: site d'annonce qui permettra aux utilisateurs de poster des annonces lorsqu'ils recherchent un animal
  * ou lorqu'ils ont trouvé un animal perdu.
  *
  * ODAKA M. et Myaz A.|| CFPT-I || IFDA-P2C
- * 
+ *
  * Date: 26.04.2020
  -->
+ <?PHP
+//Récupéréer les champs
+ ?>
  <!DOCTYPE html>
-
 <html lang="fr">
     <head>
-        <title>Accueil sans login</title>
+        <title>Accueil</title>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+        <link href="css/cssAccueil.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <script></script>
-    </body>
-  
-</html>
+        <main>
+          <header>
+            <img class="logo"/>
+            <h1></h1>
+            <p class="slogan"></p>
+          </header>
+          <nav>
+            <input type="" name="etat"/>
+            <input type="" name="espèce"/>
+            <input type="" name="ville"/>
+            <input type="" name="npa"/>
+            <input class="validerRecherche" type="submit" name="validerRecherche" value="Chercher"/>
+          </nav>
+          <div class="articles">
+          <?PHP
+          //Trouver nombre annonces
+          //afficher les annonces de manière dynamique en cherchant les informations dans la BD
+          ?>
 
+          </div>
+        </main>
+    </body>
+</html>
