eeb7674b97b6ef2f6becfd29d7f28846a5c273c4
Aliya Mz <aliya.mz@eduge.ch>
2020-05-07 00:17:11 +0200

    css et html de la page d'accueil

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <!--
  * Projet: Animaux Perdus|| M426
- * 
- * Description: site d'annonce qui permettra aux utilisateurs de poster des annonces lorsqu'ils recherchent un animal 
+ *
+ * Description: site d'annonce qui permettra aux utilisateurs de poster des annonces lorsqu'ils recherchent un animal
  * ou lorqu'ils ont trouvé un animal perdu.
  *
  * ODAKA M. et Myaz A.|| CFPT-I || IFDA-P2C
- * 
+ *
  * Date: 26.04.2020
  -->
  <!DOCTYPE html>
 
 <html lang="fr">
     <head>
-        <title>Accueil sans login</title>
+        <title>Accueil</title>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+
     </head>
     <body>
         <script></script>
     </body>
   
 </html>
-
